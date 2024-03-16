682d9329e844101ec707715290ee48c0b79f3250
autonomuscoder <mahiabirgit@tutanota.com>
2024-03-16 16:53:51 +0600

    added mini.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,6 +65,9 @@ vim.opt.rtp:prepend(lazypath)
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
+  opts = {
+    change_detection = { enabled = false }
+  },
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
@@ -86,7 +89,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      -- { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
